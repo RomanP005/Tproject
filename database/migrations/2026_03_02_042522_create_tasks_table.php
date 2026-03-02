bb87a61dc8d4a6e91b01e02769e0835afa95169d
RomanP005 <petrov231205@gmail.com>
2026-03-02 15:00:54 +0300

    Create 2026_03_02_042522_create_tasks_table.php
    addded the filling in tasks table

---
+++ database/migrations/2026_03_02_042522_create_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['new', 'job', 'success'])->default('new');
+            $table->unsignedTinyInteger('priority')->default(1);
+            $table->date('deadline')->nullable();
+            $table->foreignId('project_id')->constrained('projects')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('assignee_id')->nullable()->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
