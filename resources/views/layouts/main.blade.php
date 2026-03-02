c8d244c9091dd0be37d5b30c2e539aa4c7883aed
RomanP005 <petrov231205@gmail.com>
2026-03-02 14:58:54 +0300

    Create main.blade.php
    add layouts.main

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') - Таск-трекер на Laravel</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
+<body class="bg-light">
+
+@include('partials.navbar')
+
+<section class="py-5">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between mb-4">
+            <h2 class="mb-4">@yield('title')</h2>
+        </div>
+        <div class="p-5 bg-white rounded-4 shadow-sm">
+            @yield('body')
+        </div>
+    </div>
+</section>
+
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
