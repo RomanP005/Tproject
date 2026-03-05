7297308302d76093e757dc4cd032e52ce4cdbc66
RomanP005 <202592803+Koteyikin@users.noreply.github.com>
2026-03-05 15:46:32 +0300

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Enums\StatusEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
+    use SoftDeletes, HasFactory;
     protected $fillable = [
         'title',
         'description',
@@ -20,6 +24,7 @@ class Task extends Model
     {
         return [
           'deadline' => 'datetime',
+            'status' => StatusEnum::class,
         ];
     }
     public function project()
@@ -30,19 +35,19 @@ class Task extends Model
     {
         return $this->hasMany(TaskStatusLog::class);
     }
-    public function users()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
-    public function setStatusAttribute($value)
-    {
-        $allowedTransitions = [
-            'new' => ['job'], 'job' => ['success'], 'success' => [],
-        ];
-        $old = $this->status ?? 'new';
-        if ($old !== $value && !in_array($value, $allowedTransitions[$old] ?? [])) {
-            throw new \Exception("Invalid status transition from {$old} to {$value}");
-        }
-        $this->attributes['status'] = $value;
-    }
+//    public function setStatusAttribute($value)
+//    {
+//        $allowedTransitions = [
+//            'new' => ['job'], 'job' => ['success'], 'success' => [],
+//        ];
+//        $old = $this->status ?? 'new';
+//        if ($old !== $value && !in_array($value, $allowedTransitions[$old] ?? [])) {
+//            throw new \Exception("Invalid status transition from {$old} to {$value}");
+//        }
+//        $this->attributes['status'] = $value;
+//    }
 }
