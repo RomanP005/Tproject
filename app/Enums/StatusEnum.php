b527e92fe015afba6d42fb339a93e929053187da
RomanP005 <202592803+Koteyikin@users.noreply.github.com>
2026-03-05 15:41:37 +0300

    Update status.enum
    updated status enum

--- app/Enums/StatusEnum.php
+++ app/Enums/StatusEnum.php
@@ -5,11 +5,16 @@ namespace App\Enums;
 enum StatusEnum: string
 
 {
-    case new = "Новый";
+    case NEW = 'Новый';
 
-    case job = "В работе";
+    case JOB = 'В работе';
 
-    case success = "Выполнен";
+    case SUCCESS = 'Выполнена';
 
+
+public static function values(): array
+{
+    return array_column(self::cases(), "value");
+}
 
 }
