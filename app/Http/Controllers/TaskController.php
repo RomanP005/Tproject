442353012ee81d4a4afdab248838ef24c25c526e
RomanP005 <202592803+Koteyikin@users.noreply.github.com>
2026-03-05 15:45:16 +0300

    Create TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::where('user_id', Auth::id())->orderBy('status')->orderBy('priority')->orderBy('deadline')->get();
+        return view('tasks', ['tasks' => $tasks]);
+    }
+
+    public function create()
+    {
+
+    }
 }
