28b66507210c9a2264a2939b4091358b707d30cb
RomanP005 <202592803+Koteyikin@users.noreply.github.com>
2026-03-05 15:46:35 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,7 +36,7 @@ class User extends Authenticatable
     }
     public function tasks()
     {
-        return $this->hasMany(Task::class, 'assignee_id');
+        return $this->hasMany(Task::class);
     }
     public function taskstatuslogs()
     {
