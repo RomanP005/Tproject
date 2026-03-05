f3d9434cbec1c3348cf0fa143164e52b2552ca79
RomanP005 <202592803+Koteyikin@users.noreply.github.com>
2026-03-05 15:42:41 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,24 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('home'); })->name('home');
+Route::redirect('/', '/tasks');
 
-// Гостевые маршруты (доступны только неавторизованным)
-Route::middleware('guest')->group(function () {
-    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-    Route::post('/register', [RegisterController::class, 'register'])->name('register.store');
-
-    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [LoginController::class, 'login'])->name('login.store');
-});
-
-// Защищённые маршруты (только для авторизованных)
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::prefix('/tasks')->name('tasks.')->group(function () {
+        Route::get('/', [TaskController::class, 'index'])->name('index');
+    });
+
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterController::class, 'Registration'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register'])->name('register.store');
+
+    Route::get('/login', [LoginController::class, 'Logination'])->name('login');
+    Route::post('/login', [LoginController::class, 'login'])->name('login.store');
 });
