1409b3b14d09ee6831cfa6a6852ab91d7e5e52ee
RomanP005 <petrov231205@gmail.com>
2026-03-10 18:01:10 +0500

    Update TaskObserver.php
    изменил обсервер для задач

--- app/Observers/TaskObserver.php
+++ app/Observers/TaskObserver.php
@@ -4,40 +4,30 @@ namespace App\Observers;
 
 use App\Models\Task;
 use App\Models\TaskStatusLog;
+use App\Notifications\TaskAssignedNotification;
 use Illuminate\Support\Facades\Auth;
 
 class TaskObserver
 {
     public function created(Task $task): void
     {
-        //
+
+        if ($task->assignee_id) {
+            $task->assignee->notify(new TaskAssignedNotification($task));
+        }
     }
 
     public function updated(Task $task): void
     {
-        if($task->isDirty('status'))
-        {
+        if ($task->isDirty('status')) {
             TaskStatusLog::create([
-               'task_id' => $task->id,
-               'user_id' => Auth::id(),
-               'old_status' => $task->getOriginal('status'),
-               'new_status' => $task->status,
+                'task_id'    => $task->id,
+                'user_id'    => Auth::id(),
+                'old_status' => $task->getOriginal('status'),
+                'new_status' => $task->status->value,
             ]);
         }
+        $task->assignee->notify(new TaskAssignedNotification($task));
     }
 
-    public function deleted(Task $task): void
-    {
-        //
-    }
-
-    public function restored(Task $task): void
-    {
-        //
-    }
-
-    public function forceDeleted(Task $task): void
-    {
-        //
-    }
 }
