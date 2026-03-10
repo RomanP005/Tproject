bff5d0990c68bbf7ecf9c63a11f70777956c6867
RomanP005 <petrov231205@gmail.com>
2026-03-10 18:03:54 +0500

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('/login');
-        $middleware->redirectUsersTo('/tasks');
+        $middleware->redirectUsersTo('/tasks/index');
     })
 
     ->withMiddleware(function (Middleware $middleware) {
