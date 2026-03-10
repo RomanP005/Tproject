e84b15b0f13d6df17fe543463ec69caac56341e0
RomanP005 <petrov231205@gmail.com>
2026-03-10 18:03:20 +0500

    Update DatabaseSeeder.php
    исправил Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,25 +5,24 @@ namespace Database\Seeders;
 use App\Models\Project;
 use App\Models\Task;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     public function run(): void
     {
         $users = User::factory(7)->create();
 
-        $users->each(function ($user) {
+        $users->each(function ($user) use ($users) {
             $projects = Project::factory(1)->create([
-                'user_id' => $user->id,
+                'creator_id' => $user->id,
             ]);
-            $projects->each(function ($project) use ($user) {
+
+            $projects->each(function ($project) use ($user, $users) {
                 Task::factory(2)->create([
                     'project_id' => $project->id,
-                    'user_id' => $user->id,
+                    'user_id'    => $user->id,
+                    'assignee_id' => $users->random()->id,
                 ]);
             });
         });
